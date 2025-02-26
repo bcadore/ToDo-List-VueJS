8adeb4321b86691bb4a98e33197cec7584dd360a
Bernardo Cadore <bernardo.cadore95@gmail.com>
2025-02-25 21:50:44 -0300

    Estilização inicial.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+      <h1>Minhas tarefas</h1>
+      <p>Você possui 7 tarefas pendentes</p>
+    </header>
+    <form>
+      <div class="row">
+        <div class="col">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Digite a descrição da tarefa"
+          />
+        </div>
+        <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+        <div class="col-md-3">
+          <select class="form-control">
+            <option value="todas">Todas tarefas</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="finalizadas">Finalizadas</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <ul class="list-group mt-4">
+      <li class="list-group-item">
+        <input type="checkbox">
+        <label class="ms-3" for="">
+          Estudar programação
+        </label>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
